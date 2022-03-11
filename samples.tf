4753c9d2d5bde220686643b8564ebee9a7a60cd9
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2022-03-11 18:00:00 +0530

    complex variables

---
+++ samples.tf
@@ -0,0 +1,18 @@
+# Lists
+
+variable "a" {
+    type = list(string)
+    default = ["foo", "bar", "baz"]
+}
+
+output "a" {
+    value = var.a
+}
+
+output "b" {
+    value = element(var.a, 1)
+}
+
+output "c" {
+    value = length(var.a)
+}
