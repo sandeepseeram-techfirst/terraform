d1d35b029549d403906bf4c15ccf065e8bcf205d
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2022-05-18 19:13:53 +0530

    lifecycle  added

--- lifecycle.tf
+++ lifecycle.tf
@@ -9,3 +9,16 @@ lifecycle {
   prevent_destroy = true
 }
 */
+
+
+
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+resource "aws_sqs_queue" "queue" {
+  name = "queue"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
